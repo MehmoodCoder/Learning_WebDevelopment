5ef583cb3978dbc59604e00db9086a807ac886c0
MH_MAC <mh_mac@MACs-MacBook-Pro.local>
2025-10-12 20:55:57 +0500

    It is a code of Chapter 2 practice set with Harry

---
+++ Learning_withHarry_JS/Practice_sets/ch2.js
@@ -0,0 +1,37 @@
+// Q no 1 :
+
+let age = -1;
+console.log(age);
+if (age > 10 && age < 20) {
+   console.log("Your age is lying between 10 and 20 and your age = ", age);
+}
+if (age < 0) {
+   console.log("Invalid age !");
+} else {
+   console.log("Your age is not lying between 10 and 20  and your age = ", age);
+}
+
+// Q no 2 : by Codewithharry chennal
+
+// Q no 3 :
+
+let n = 12;
+if (n % 2 == 0 && n % 3 == 0) {
+   console.log("The number is divisible by 2 and 3");
+} else {
+   console.log("The number is not divisible by 2 and 3");
+}
+
+// Q no 4 :
+
+let n1 = 17;
+if (n1 % 2 == 0 || n1 % 3 == 0) {
+   console.log("The number is divisible by 2 or 3");
+} else {
+   console.log("The number is not divisible by 2 or 3");
+}
+
+// Q no 5 :
+
+let age1 = 19;
+console.log(age1 >= 18 ? "You can drive" : "you cann't drive");
