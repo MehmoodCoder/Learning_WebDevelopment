6ec6af1b83969ba68f3864875c3ba3b64370ebe0
MH_MAC <mh_mac@MACs-MacBook-Pro.local>
2025-10-12 20:55:31 +0500

    It is a code of Chapter 1 practice set with Harry

---
+++ Learning_withHarry_JS/Practice_sets/ch1.js
@@ -0,0 +1,48 @@
+// Q no 1 :
+
+let a = "hello world "
+let b = 90
+console.log(a+b)
+
+// Q no 2 :
+
+console.log(typeof (a+b) )
+
+// Q no 3 :
+
+const objects = {
+   Name : "Mehmood",
+   Age : 16,
+   Result : 83 ,
+   Section : "A"
+}
+
+//objects = "hi hi hi" //  comment this due to error   // can't change 
+
+// Q no 4 :
+
+const object = {
+   Name : "Mehmood",
+   Age : 16,
+   Result : 83 ,
+   Section : "A"
+}
+
+object['Friend'] = "Ab"
+object['Age'] = 17
+
+console.log(object)
+
+// Q no 5 :
+
+const dict = {
+   yakka : "hard work ,work",
+   diligent : "A diligent person always works hard and pays attention to detail",
+   courage : "Courage means having the strength to face fear or difficulties",
+   kind : "A kind person helps others and speaks gently",
+   loyalty : "Loyalty means staying true and faithful to someone or something",
+   wisdom : "Wisdom is the ability to use knowledge and experience to make good decisions"
+}
+
+console.log(dict.yakka) // to particular once // console.log(dict['yakka']) same as 
+console.log(dict)
